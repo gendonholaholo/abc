9b2e40b1025bad7edf1649bd2100d3f2d2684c1f
Mas Gendon <67081785+gendonholaholo@users.noreply.github.com>
2025-03-25 19:23:31 +0700

    abc

--- scripts/encrypt.ps1
+++
@@ -1,8 +0,0 @@
-$inputText = [Console]::In.ReadToEnd()
-$bytes = [System.Text.Encoding]::UTF8.GetBytes($inputText)
-$Encrypted = [System.Convert]::ToBase64String(
-    [System.Security.Cryptography.ProtectedData]::Protect(
-        $bytes, $null, [System.Security.Cryptography.DataProtectionScope]::CurrentUser
-    )
-)
-[Console]::Out.WriteLine($Encrypted)
